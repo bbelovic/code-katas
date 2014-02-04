ed25bd1ae3f140e7a6028d16d63c7712f21a7521
Boris Belovic <bbelovic@gmail.com>
2014-02-04 23:05:15 +0100

    Added more test case for compute() method.

--- src/test/java/org/bbelovic/codekatas/NumberInTriangleKataTest.java
+++ src/test/java/org/bbelovic/codekatas/NumberInTriangleKataTest.java
@@ -1,5 +1,6 @@
 package org.bbelovic.codekatas;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.assertEquals;
@@ -7,12 +8,22 @@ import static org.testng.Assert.assertEquals;
 @Test
 public class NumberInTriangleKataTest {
 
-    @Test
-    public void testCompute() throws Exception {
-        final Triangle triangle = new TriangleSerializer().deserialize("./src/test/resources/triangle.txt");
+    private static final String RESOURCES_DIR = "./src/test/resources/";
+
+    @Test(dataProvider = "inputDataAndExpectedResult")
+    public void testCompute(final String inputFile, final int expectedResult) throws Exception {
+        final Triangle triangle = new TriangleSerializer().deserialize(inputFile);
         final NumberInTriangleKata kata = new NumberInTriangleKata(triangle);
         final int actual = kata.compute();
-        assertEquals(actual, 27);
+        assertEquals(actual, expectedResult);
+    }
+
+    @DataProvider(name = "inputDataAndExpectedResult")
+    public Object [][] computeDataProvider() {
+        return new Object [][] {{RESOURCES_DIR + "triangle1.txt", 7},
+                                {RESOURCES_DIR + "triangle2.txt", 1},
+                                {RESOURCES_DIR + "triangle3.txt", 4}};
+
     }
 
 }
