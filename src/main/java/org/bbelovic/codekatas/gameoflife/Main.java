1a2841bef814b2d72505c75ded31c9c1a08b2ccf
Boris Belovic <bbelovic@gmail.com>
2014-04-27 21:03:05 +0200

    Added text file with blinker pattern as an input pattern for testing application

--- src/main/java/org/bbelovic/codekatas/gameoflife/Main.java
+++ src/main/java/org/bbelovic/codekatas/gameoflife/Main.java
@@ -3,19 +3,13 @@ package org.bbelovic.codekatas.gameoflife;
 import java.io.IOException;
 
 public class Main {
+    private static final String BLINKER_PATTERN_FILE = "./src/main/resources/gameoflife/blinker.txt";
     public static void main(String[] args) throws IOException {
-        final Cell [][] initCells = new Cell[][] {
-                {new Cell(false), new Cell(false), new Cell(false), new Cell(false)},
-                {new Cell(false), new Cell(true), new Cell(true), new Cell(false)},
-                {new Cell(false), new Cell(true), new Cell(true), new Cell(false)},
-                {new Cell(false), new Cell(false), new Cell(false), new Cell(false)}
-        };
-        Grid grid = GridSerializer.deserializeFromFile("./blinker.txt");
+        Grid grid = GridSerializer.deserializeFromFile(BLINKER_PATTERN_FILE);
         Transformer transformer = new Transformer();
         for (int i = 0; i < 10; i++) {
             grid = transformer.transform(grid);
             GridSerializer.serializeToFile(grid, "./blinker-"+ i + ".txt");
         }
-        System.out.println(grid);
     }
 }
